169b595633650c878b4f0a1cf3ea3b1da835934b
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-02-22 10:37:56 -0600

    Se agregan comentarios a setup

--- Reto1/courseworks/setup.py
+++ Reto1/courseworks/setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+#Se le pone el nombre de paquete para concatenar.
 package_name = 'courseworks'
 
+#Se agrega el setup para poder trabajar con los archivos.
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    #Archivos de datos
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -18,6 +21,7 @@ setup(
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
+    #Se agregan archivos necesarios.
     entry_points={
         'console_scripts': [
             'signal_generator = courseworks.signal_generator:main',
