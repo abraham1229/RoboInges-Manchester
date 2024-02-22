a3a6d3efc10b1fab1de30468f093201602a9a7b6
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-02-22 10:58:01 -0600

    Se agrega comentario al process

--- Reto1/courseworks/courseworks/signal_process.py
+++ Reto1/courseworks/courseworks/signal_process.py
@@ -3,23 +3,30 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
 
+#Se crea el nodo SignalProcessor tomando objeto de Node.
 class SignalProcessor(Node):
 
+    #Se inicializa el nodo
     def __init__(self):
+        #Se crear el nodo que será encargado del proceso de la senial
         super().__init__('process')
+        #Se hace la subscripcion a el topico signal
         self.subscription_signal = self.create_subscription(
             Float32,
             'signal',
             self.signal_callback,
             10)
+        #Se hace la subscripcion al topico time
         self.subscription_time = self.create_subscription(
             Float32,
             'time',
             self.time_callback,
             10)
-        self.publisher_proc_signal = self.create_publisher(Float32, 'proc_signal', 10)  # Crear publicador para proc_signal
+        # Crear publicador para proc_signal
+        self.publisher_proc_signal = self.create_publisher(Float32, 'proc_signal', 10)  
         self.alpha = 10.0  # Valor inicial del offset
-        self.phase_shift = 20.0  # Valor inicial del cambio de fase
+        self.phase_shift = 20.0  # Valor inicial del cambio de fase}
+        #Se manda mensaje de creación exitosa
         self.get_logger().info('Signal Processor node successfully initialized!!!')
 
     def signal_callback(self, msg):
@@ -38,10 +45,15 @@ class SignalProcessor(Node):
         # No es necesario hacer nada con el tiempo en este nodo
         pass
 
+#La función que será llamada según nuestro setup
 def main(args=None):
+    #Se inicializa el entorno de ros
     rclpy.init(args=args)
+    #Se crear una instancia de la clase creada previamente
     signal_processor = SignalProcessor()
+    #Creación de bucle de eventos ros2
     rclpy.spin(signal_processor)
+    #Se liberan recursos como buena práctica
     signal_processor.destroy_node()
     rclpy.shutdown()
 
