02d56bb795081136a9d83b7977e6ba775595b46d
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-02-26 10:37:01 -0600

    Se cambian las frecuencias

--- Reto2/src/params_reto/params_reto/signal_gener.py
+++ Reto2/src/params_reto/params_reto/signal_gener.py
@@ -7,29 +7,33 @@ class My_Talker_Params(Node):
     def __init__(self):
         super().__init__('ri_signal_generator')
         self.declare_parameter('ri_type', 1)
-        self.pub = self.create_publisher(Float32, 'ri_signal', 10)
-        timer_period = 0.02
+        self.pub = self.create_publisher(Float32, 'ri_signal', 1000)  # Frecuencia de publicación de 1kHz
+        timer_period = 0.001  # Período de temporizador para 1kHz
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('Talker params node initialized')
 
     def timer_callback(self):
         ri_type = self.get_parameter('ri_type').get_parameter_value().integer_value
-        signal_value = 0.0
+        time = self.get_clock().now().to_msg().nanosec / 1e9  # Convertir a segundos
         if ri_type == 1:
-            # Generar una señal senoidal con frecuencia de 1 Hz y amplitud de 1.0
-            signal_value = math.sin(self.get_clock().now().to_msg().sec)
+            # Generar una señal senoidal de 2 Hz y amplitud de 0.5
+            signal_value = 0.5 * math.sin(2 * math.pi * 2 * time)
         elif ri_type == 2:
             # Generar un pulso cuadrado
             signal_value = 1.0 if (self.get_clock().now().to_msg().sec % 2) < 1 else 0.0
         elif ri_type == 3:
             # Generar una señal de diente de sierra
-            signal_value = (self.get_clock().now().to_msg().sec % 10) / 10.0
+            signal_value = (time % 10) / 10.0
         elif ri_type == 4:
-            # Generar una señal coseno con frecuencia de 1 Hz y amplitud de 1.0
-            signal_value = math.cos(self.get_clock().now().to_msg().sec)
+            # Generar una señal coseno de 2 Hz y amplitud de 0.5
+            signal_value = 0.5 * math.cos(2 * math.pi * 2 * time)
         elif ri_type == 5:
             # Generar una señal tangente
-            signal_value = math.tan(self.get_clock().now().to_msg().sec)
+            signal_value = math.tan(time)
+        else:
+            # Si el tipo no es reconocido, generar una señal senoidal de 2 Hz y amplitud de 0.5 por defecto
+            signal_value = 0.5 * math.sin(2 * math.pi * 2 * time)
+
         # Publicar el valor de la señal
         msg = Float32()
         msg.data = signal_value
