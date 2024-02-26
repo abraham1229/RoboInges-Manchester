2eb5fd983b9c923e0d761f5d44a5e643823002cb
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-02-25 18:06:02 -0600

    Se agrega cambio para detectar diferente parametros

--- Reto2/build/params_reto/build/lib/params_reto/signal_gener.py
+++ Reto2/build/params_reto/build/lib/params_reto/signal_gener.py
@@ -1,21 +1,31 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Int32
+from std_msgs.msg import String, Int32, Float32
+import math
 
 class My_Talker_Params(Node):
     def __init__(self):
-        super().__init__('roboinges_signal_generator')
+        super().__init__('ri_signal_generator')
         self.declare_parameter('ri_type', 1)
-        self.pub = self.create_publisher(Int32, 'signal', 10)
-        timer_period = 1.0
+        self.pub = self.create_publisher(String, 'ri_signal', 10)
+        timer_period = 0.02
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info('Talker params node initialized')
-        self.msg = Int32()
+        self.msg_type = String()
+        self.time = Int32()
+        self.time = 0.0
+
 
     def timer_callback(self):
         ri_type = self.get_parameter('ri_type').get_parameter_value().integer_value
-        self.msg.data = ri_type
-        self.pub.publish(self.msg)
+        if(ri_type == 1):
+            self.msg_type.data = "Hola"
+            self.pub.publish(self.msg_type)
+        else:
+
+            self.msg_type.data = "Waa"
+            self.pub.publish(self.msg_type)
+
 
 def main(args = None):
     rclpy.init(args=args)
